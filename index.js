67c88bdbfc143cfc9c31dff92edd58cdf05741a3
nickfrahm <nfrahm24@gmail.com>
2021-04-23 19:56:02 -0500

    fixed grid generation

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 //default grid to 16x16
-let squares = 4;
+let squares = 32;
 
 //populate grid via squares
 function populateGrid(numSquares) {
     const width = document.getElementById('grid').clientWidth;
     const height = document.getElementById('grid').clientWidth;
-    const squareWidth = width/numSquares;
-    const squareHeight = height/numSquares;
+    const squareSize = (1/squares*100);
 
-    console.log(`w: ${width}`, `h: ${height}`, `sw: ${squareWidth}`, `sh: ${squareHeight}`);
+    console.log(`w: ${width}`, `h: ${height}`, `squareSize: ${squareSize}`);
 
     for (let j = 0; j < numSquares; j++) {
         console.log("row " + (j+1))
         for (let i = 0; i < numSquares; i++) {
             let div = document.createElement("div");
-            div.style.width = (squareWidth - squares).toString() + "px";
-            div.style.height = (squareHeight - squares).toString() + "px";
+            div.style.width = squareSize + "%";
+            div.className = "square square:after";
             div.style.border = "1px solid black";
             div.style.display = 'inline-block';
             div.style.padding = '0';
